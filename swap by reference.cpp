d954caf7f9e6551deec19802e976c08d09b8f98a
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-08-22 11:18:14 +0530

    Update swap by reference.cpp

--- swap by reference.cpp
+++ swap by reference.cpp
@@ -1,4 +1,25 @@
-
+/*
+Name: Dhruv Jain
+PRN: 24070123042
+Batch: A2
+*/
+#include <iostream>
+using namespace std;
+void swap(int *x, int *y){
+  int t;
+  t=*x;
+  *x=*y;
+  *y=t;
+}
+int main(){
+  int a=4, b=2;
+  cout<<"Value of a before swapping is: "<<a<<endl;
+  cout<<"Value of b before swapping is: "<<b<<endl;
+  swap(&a,&b);
+  cout<<"Value of a after swapping is: "<<a<<endl;
+  cout<<"Value of b after swapping is: "<<b<<endl;
+  return 0;
+}
 
 /*
 Output:
