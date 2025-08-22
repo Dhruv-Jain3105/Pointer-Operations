29f468a931d8a5435d675a5792017ee573654864
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-08-22 11:06:49 +0530

    Create swap by value.cpp

---
+++ swap by value.cpp
@@ -0,0 +1,23 @@
+/*
+Name: Dhruv Jain
+PRN: 24070123042
+Batch: A2
+*/
+#include <iostream>
+using namespace std;
+void swap(int x, int y){
+  int swap;
+  swap=x;
+  x=y;
+  y=swap;
+}
+int main(){
+  int a=5, b=2
+  swap(a,b);
+  cout<<"Value of a is: "<<a<<endl;
+  cout<<"Value of b is: "<<b<<endl;
+  return 0;
+}
+
+/*
+Output:
