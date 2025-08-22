a707df109c7678397fffa7904e98cca1955ec4df
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-08-22 11:16:44 +0530

    Update swap by value.cpp

--- swap by value.cpp
+++ swap by value.cpp
@@ -5,19 +5,26 @@ Batch: A2
 */
 #include <iostream>
 using namespace std;
-void swap(int x, int y){
-  int swap;
-  swap=x;
+void swap(int *x, int *y){
+  int *t;
+  t=x;
   x=y;
-  y=swap;
+  y=t;
 }
 int main(){
-  int a=5, b=2
+  int a=4, b=2;
+  cout<<"Value of a before swapping is: "<<a<<endl;
+  cout<<"Value of b before swapping is: "<<a<<endl;
   swap(a,b);
-  cout<<"Value of a is: "<<a<<endl;
-  cout<<"Value of b is: "<<b<<endl;
+  cout<<"Value of a after swapping is: "<<a<<endl;
+  cout<<"Value of b after swapping is: "<<b<<endl;
   return 0;
 }
 
 /*
 Output:
+Value of a before swapping is: 4
+Value of b before swapping is: 4
+Value of a after swapping is: 2
+Value of b after swapping is: 4
+*/
